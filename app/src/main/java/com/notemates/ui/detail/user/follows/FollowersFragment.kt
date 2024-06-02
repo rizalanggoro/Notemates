7c5b07b8b1f680c88cae345e92450c89d20aeb26
Latihan Git <melonpea12@gmail.com>
2024-06-02 15:22:34 +0700

    first commit

--- app/src/main/java/com/notemates/ui/detail/user/follows/FollowersFragment.kt
+++ app/src/main/java/com/notemates/ui/detail/user/follows/FollowersFragment.kt
@@ -17,7 +17,6 @@ class FollowersFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_followers, container, false)
     }
 
